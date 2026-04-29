c9e728bf67459df8dc456c2d74a883df76d1d2d4
minmul <sksmstmdrud@gmail.com>
2026-04-29 19:22:13 +0900

    Refactor: ValidateAccountUseCase 내 비밀번호 유효성 검사 로직 간소화
    - 첫 문자 영문 제한 및 특수문자 종류 체크 제거
    - 8자 이상 및 공백 미포함 조건으로 변경
    - 이유: 도서관 임시 비밀번호의 경우 8자리 영문 대소문자로 정해지며, 이 경우에도 KW Pass 이용이 가능하도록 변경

--- kwpass-phone/src/main/java/minmul/kwpass/domain/usecase/ValidateAccountUseCase.kt
+++ kwpass-phone/src/main/java/minmul/kwpass/domain/usecase/ValidateAccountUseCase.kt
@@ -4,22 +4,7 @@ import javax.inject.Inject
 
 class ValidateAccountUseCase @Inject constructor() {
     fun isValidPassword(ps: String): Boolean {
-        //첫문자를 영문자로 입력하세요.
-        //영대문자(A~Z), 영소문자(a~z), 숫자(0~9) 및 특수문자(32개)
-        //중 3종류 이상으로 입력하세요. 8자리 이상
-        if (ps.length < 8) {
-            return false
-        }
-
-        if (!ps[0].isLetter()) {
-            return false
-        }
-
-        val specialChars = "!\"#\$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-
-        return ps.all { char ->
-            char.isLetterOrDigit() || specialChars.contains(char)
-        }
+        return ps.length >= 8 && ps.none { it.isWhitespace() }
     }
 
     fun isValidRid(input: String): Boolean {
